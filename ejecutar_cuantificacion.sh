1f095f84f175038c4d7fd6dfbc4f4128da55d3fd
Miguel <mlagunasanz@gmail.com>
2026-05-05 12:37:28 +0000

    Añadido script de alineamiento y cuantificación con STAR/RSEM

---
+++ ejecutar_cuantificacion.sh
@@ -0,0 +1,7 @@
+rsem-calculate-expression \
+  --star \
+  --star-gzipped-read-file \
+  -p 8 \
+  /home/mlaguna2022/CNIC/resultados_limpios-20260421T100617Z-3-001/resultados_limpios/S1_SKA_1__MichaelaVeliova_RNA_Seq_Directional_S1_R1_001_trimmed.fq.gz \
+  /home/mlaguna2022/referencia_raton/indice_rsem/raton \
+  /home/mlaguna2022/CNIC/resultados_alineamiento/S1_SKA_1_res
